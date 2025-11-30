56aba8223bf17e20c49142301ba5a4be29afeb57
Catzeus <marcosalex9061@gmail.com>
2025-11-30 20:22:23 -0300

    palavras

--- v1/app.js
+++ v1/app.js
@@ -1,9 +1,21 @@
 const express =require('express');
+const cron = require('node-cron')
+
+
 
 const app = express();
 
+
+
 app.use(express.json())
 
+cron.schedule("17 17 * * *",()=>{
+    console.log('Rodando script diario')
+})
 
-const PORT = 3000
-app.listen()
+
+const PORT = 3000 || process.env.PORT;
+app.listen(PORT,()=>{
+    console.log(`rodando em http://localhost:${PORT}`);
+
+});
